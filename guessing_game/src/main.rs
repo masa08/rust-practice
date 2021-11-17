21f4995d005db8f16bca3bb5ae763c7664941db1
masa08 <ushimasarug08@gmail.com>
2021-11-17 09:08:38 +0900

    :memo: add memo

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,8 +4,15 @@ fn main() {
     println!("Guess the number!");          // 数を当ててごらん
     println!("Please input your guess.");   // ほら、予想を入力してね
 
+    // MEMO
+    // let hoge     => immutable
+    // let mut hoge => mutable
     let mut guess = String::new();
 
+    // MEMO
+    // Result型 => ok or errorを返却する
+    // Expectはokであれば値を返却し
+    // errorであればerror messageを返却する
     io::stdin().read_line(&mut guess)
     .expect("Failed to read line");     // 行の読み込みに失敗しました
 
