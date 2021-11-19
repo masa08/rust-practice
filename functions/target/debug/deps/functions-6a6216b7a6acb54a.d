51a3eb64563a3370eccbc5c4ea23771f6e365a89
masa08 <ushimasarug08@gmail.com>
2021-11-19 09:34:50 +0900

    :sparkles: finish function

---
+++ functions/target/debug/deps/functions-6a6216b7a6acb54a.d
@@ -0,0 +1,5 @@
+/Users/masatakaushijima/Desktop/experimental/rust-practice/functions/target/debug/deps/functions-6a6216b7a6acb54a: src/main.rs
+
+/Users/masatakaushijima/Desktop/experimental/rust-practice/functions/target/debug/deps/functions-6a6216b7a6acb54a.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/masatakaushijima/Desktop/experimental/rust-practice/functions/target/debug/functions: /Users/masatakaushijima/Desktop/experimental/rust-practice/functions/src/main.rs
