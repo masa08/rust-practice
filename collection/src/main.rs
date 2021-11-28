8c100ce3f297ca545f882aedd790d206aa78c772
masa08 <ushimasarug08@gmail.com>
2021-11-28 10:57:02 +0900

    :sparkles: collection

---
+++ collection/src/main.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    // for (key, value) in &scores {
+    //     println!("{}: {}", key, value);
+    // }
+
+    // let team_name = String::from("Blue");
+    // let score = scores.get(&team_name);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+}
